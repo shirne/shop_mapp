c112156cf2b8164c3329702f1e4a7d39f9cee92f
shirne <shirne@126.com>
2018-12-24 10:38:13 +0800

    地址功能调试，readme增加截图

--- README.md
+++ README.md
@@ -6,11 +6,21 @@
 
  - [x] 首页,产品页，新闻页
  - [x] 授权登录
- - [x] 产品详情……开发中
- - [ ] 购物车
+ - [x] 产品详情
+ - [x] 购物车
+ - [x] 会员中心
+ - [x] 收货地址
  - [ ] 下单支付
- - [ ] 会员中心
  - [ ] 订单管理
 
 # 后端项目
 [ShirneCMS](https://gitee.com/shirnecn/ShirneCMS)
+
+# 功能截图
+![首页](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-index.png "首页")
+![产品展示](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-list.png "产品展示")
+![购物车](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-cart.png "购物车")
+![购物车编辑](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-cart-2.png "购物车编辑")
+![会员中心](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-member.png "会员中心")
+![地址管理](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-address-list.png "地址管理")
+![地址编辑](https://shirne.oss-cn-shenzhen.aliyuncs.com/website-mapp/shop-address.png "地址编辑")
--- pages/member/address-add.js
+++ pages/member/address-add.js
@@ -5,181 +5,177 @@ var util = require("../../utils/util.js");
 const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    action:"添加",
-    address_id:0,
-    address:{},
-    area:[],
-    isloading:false
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        action: "添加",
+        address_id: 0,
+        address: {},
+        area: [],
+        isloading: false
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    app.initShare(null)
-    if(options.address_id){
-      this.setData({
-        action:"修改",
-        address_id:options.address_id
-      })
-      app.httpPost('user/addressget',{address_id:options.address_id},(data)=>{
-        if(data.status==0){
-          var areas=data.data.address.area.split('&nbsp;')
-          data.data.address.area_array=areas
-          data.data.area=areas
-          this.setData(data.data)
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        app.initShare(null)
+        if (options.address_id) {
+            this.setData({
+                action: "修改",
+                address_id: options.address_id
+            })
+            app.httpPost('member/get_address', { id: options.address_id }, (json) => {
+                if (json.code == 1) {
+                    var areas = [json.data.province, json.data.city, json.data.area]
+                    this.setData({
+                        address: json.data,
+                        area: areas
+                    })
+                }
+            })
+        } else {
+            wx.setNavigationBarTitle({
+                title: '添加地址',
+            })
         }
-      })
-    }else{
-        wx.setNavigationBarTitle({
-            title: '添加地址',
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+    onChanged: function (e) {
+        console.log(e)
+        var address = this.data.address
+        if (e.detail.checked) {
+            address.is_default = true
+        } else {
+            address.is_default = false
+        }
+        this.setData({
+            address: address
+        })
+    },
+    checkRowTap: function () {
+        var address = this.data.address
+        if (!address.is_default) {
+            address.is_default = true
+        } else {
+            address.is_default = false
+        }
+        this.setData({
+            address: address
+        })
+    },
+    bindValueChange: function (e) {
+        var address = this.data.address
+        var k = e.currentTarget.dataset.key
+        address[k] = e.detail.value
+        this.setData({
+            address: address
+        })
+    },
+    bindAreaChange: function (e) {
+        //console.log(e)
+        var areas = e.detail.value
+        var address = this.data.address
+        address.province = areas[0]
+        address.city = areas[1]
+        address.area = areas[2]
+        this.setData({
+            address: address,
+            area: areas
+        })
+    },
+    bindTextAreaBlur: function (e) {
+        //console.log(e)
+        var address = this.data.address
+        address.address = e.detail.value
+        this.setData({
+            address: address
+        })
+    },
+    buttonComplete: function () {
+        var address = this.data.address
+        if (!address.recive_name) {
+            app.error('请填写收货人')
+            return
+        }
+        if (!address.mobile) {
+            app.error('请填写联系电话')
+            return
+        }
+        if (address.mobile.length == 11 && !util.checkMobile(address.mobile)) {
+            app.error('手机号码不正确')
+        }
+        if (!address.area) {
+            app.error('请选择所在地区')
+            return
+        }
+        if (!address.address) {
+            app.error('请填写详细地址')
+            return
+        }
+        this.setData({
+            isloading: true
+        })
+        app.httpPost('member/edit_address', { id: this.data.address_id, address: address }, (json) => {
+            if (json.code == 1) {
+                app.success('保存成功')
+                setTimeout(() => {
+                    wx.navigateBack({
+                        delta: 1
+                    })
+                }, app.globalData.tipmsgstay)
+            } else {
+                app.error(json.msg)
+            }
         })
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-  onChanged:function(e){
-    console.log(e)
-    var address = this.data.address
-    if (e.detail.checked) {
-      address.is_default = true
-    } else {
-      address.is_default = false
-    }
-    this.setData({
-      address: address
-    })
-  },
-  checkRowTap:function(){
-    var address = this.data.address
-    if (!address.is_default) {
-      address.is_default = true
-    } else {
-      address.is_default = false
-    }
-    this.setData({
-      address: address
-    })
-  },
-  bindValueChange:function(e){
-    var address = this.data.address
-    var k=e.currentTarget.dataset.key
-    address[k] = e.detail.value
-    this.setData({
-      address: address
-    })
-  },
-  bindAreaChange:function(e){
-    //console.log(e)
-    var areas=e.detail.value
-    var address=this.data.address
-    address.area=areas.join(' ')
-    address.area_array=areas
-    this.setData({
-      address:address,
-      area:areas
-    })
-  },
-  bindTextAreaBlur:function(e){
-    //console.log(e)
-    var address = this.data.address
-    address.street = e.detail.value
-    this.setData({
-      address: address
-    })
-  },
-  buttonComplete:function(){
-    var address=this.data.address
-    if(!address.recive_name){
-      app.error('请填写收货人')
-      return
-    }
-    if (!address.mobile) {
-      app.error('请填写联系电话')
-      return
-    }
-		if (address.mobile.length == 11 && !util.checkMobile(address.mobile)){
-			app.error('手机号码不正确')
-		}
-    if (!address.area) {
-      app.error('请选择所在地区')
-      return
-    }
-    if (!address.street) {
-      app.error('请填写详细地址')
-      return
-    }
-    this.setData({
-      isloading:true
-    })
-		if (this.data.address.area_array.length ==1){
-			this.data.address.area_array = this.data.address.area_array[0].split(" ")
-			this.setData({
-				address : this.data.address
-			})
-		}
-    app.httpPost('user/addressupdate',{address:address},(data)=>{
-      if(data.status==0){
-        app.success('保存成功')
-        setTimeout(()=>{
-          wx.navigateBack({
-            delta:1
-          })
-        }, app.globalData.tipmsgstay)
-      }else{
-        app.error(data.message)
-      }
-    })
-  }
 })
